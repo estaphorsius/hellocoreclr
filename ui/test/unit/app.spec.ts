409b543b26589173b5407cc4ae28fa8a397cef42
elmar <e.staphorsius@nl.ccv.eu>
2016-10-13 07:46:41 +0200

    Add unit test for App module.

--- ui/test/unit/app.spec.ts
+++ ui/test/unit/app.spec.ts
@@ -1,7 +1,16 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
 import {App} from '../../src/app';
 
 describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().message).toBe('Hello World!');
+  it('configures routing', () => {
+    var sut : App = new App();
+    var config : RouterConfiguration = new RouterConfiguration();
+    var router : Router = new Router(null, null)
+    sut.configureRouter(config, router);
+    expect(config.title).toBe('Hello Core Clr!');
+    router.configure(config).then((result:any)=>{
+      expect(router.routes[0].name).toBe('home');
+      expect(router.routes[0].moduleId).toBe('greeting/greeting');
+    });
   });
 });
